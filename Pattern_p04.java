61d12d922e52fa2c5ad81f5587112a2101b80719
harshadkshirsagar619 <harshadkshirsagar619@gmail.com>
2026-06-07 23:43:44 +0530

    Add files via upload

---
+++ Pattern_p04.java
@@ -0,0 +1,24 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class Pattern_p04 {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Pattern_p04.pattern(n);
+    }
+    public static void pattern(int n)
+    {
+        for (int i = 1;i < n ;i++)
+        {
+            for (int j = 1;j <= i;j++)
+            {
+                System.out.print(" "+i);
+            }
+            System.out.println();
+        }
+    }
+}
