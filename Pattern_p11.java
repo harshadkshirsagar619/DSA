0b68932605dc422b20d184706d5e7a9215b2b35d
harshadkshirsagar619 <harshadkshirsagar619@gmail.com>
2026-06-14 23:12:57 +0530

    Add files via upload

---
+++ Pattern_p11.java
@@ -0,0 +1,28 @@
+package Pattern;
+
+public class Pattern_p11 {
+
+    public static void main(String[] args) {
+
+        int space  = 2*(5-1);
+        for (int i = 1;i<=5;i++)
+        {
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            for (int j = 1;j <= space;j++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int j = i;j>=1;j--)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+            space -= 2;
+        }
+    }
+}
