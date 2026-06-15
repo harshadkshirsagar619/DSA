820b2738b882c9280a39822761f230c3c5b764be
harshadkshirsagar619 <harshadkshirsagar619@gmail.com>
2026-06-15 21:13:05 +0530

    Add files via upload

---
+++ Pattern_p13.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class Pattern_p13 {
+    public static void main(String[] args) {
+
+        int count = 0;
+        for (int i = 1 ;i<=5;i++)
+        {
+            for (int j = 1 ;j<=i;j++)
+            {
+                count = count + 1;
+                System.out.print(count+" ");
+            }
+            System.out.println();
+        }
+    }
+}
