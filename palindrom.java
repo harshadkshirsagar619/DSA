3b29b6411fe4843a27c0239ca2b40f8235b76ac3
harshadkshirsagar619 <harshadkshirsagar619@gmail.com>
2026-07-29 23:41:44 +0530

    Add files via upload

---
+++ palindrom.java
@@ -0,0 +1,22 @@
+package StringProject;
+
+public class palindrom {
+    public static void main(String[] args) {
+        String str = "namank";
+        System.out.println(palindrom(str));
+    }
+    static boolean palindrom(String str)
+    {
+        str = str.toLowerCase();
+        for(int i=0;i<= str.length() / 2;i++)
+        {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1 -i);
+            if(start != end)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
